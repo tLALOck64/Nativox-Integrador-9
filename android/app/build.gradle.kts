237c883b8c9a711a1e1d8d5e4f9f4bea87e596bc
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-07-26 13:09:12 -0600

    se añadio el certificado de google play

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 val keystoreProperties = Properties()
@@ -32,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.nativox2025.integrador"
+        applicationId = "com.example.integrador"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
